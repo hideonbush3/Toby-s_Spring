ed1f3ae57c8f315988b01f0eb02152bb0e38664f
hideonbush3 <ppoii0961@naver.com>
2024-07-19 18:00:22 +0900

    의존관계 주입 응용 DAO와 DB 커넥션 생성 오브젝트 사이에 DB 연결횟수 카운팅하는 객체 추가하기

---
+++ src/main/java/com/hideonbush/vol1/ch1/dao/CountingConnectionMaker.java
@@ -0,0 +1,25 @@
+package com.hideonbush.vol1.ch1.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CountingConnectionMaker implements ConnectionMaker {
+    private int counter = 0;
+
+    private ConnectionMaker realConnectionMaker;
+
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+        this.realConnectionMaker = realConnectionMaker;
+    }
+
+    @Override
+    public Connection newConnection() throws ClassNotFoundException, SQLException {
+        this.counter++;
+        return realConnectionMaker.newConnection();
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+}
---
+++ src/main/java/com/hideonbush/vol1/ch1/dao/UserDaoConnectionCouningTest.java
@@ -0,0 +1,21 @@
+package com.hideonbush.vol1.ch1.dao;
+
+import java.sql.SQLException;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class UserDaoConnectionCouningTest {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+
+        // 10회 작업
+        for (int i = 0; i < 10; i++) {
+            userDao.get("abc123");
+        }
+
+        CountingConnectionMaker counter = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.printf("userDao 사용횟수 -> %d회", counter.getCounter());
+    }
+}
