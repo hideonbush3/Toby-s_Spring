4f13ab3ce84007d7052de97a04de82f066efa4b7
hideonbush3 <ppoii0961@naver.com>
2024-07-26 14:39:56 +0900

    2-4 스프링 테스트 컨텍스트 프레임워크 같은 설정파일을 기반으로 생성하는 애플리케이션 컨텍스트를 테스트마다 생성하는건 비효율적이라서 애플리케이션 컨텍스트를 테스트 전에 하나만 생성하고 이것을 테스트들이 공유해서 사용도록 함

--- src/main/java/com/hideonbush/vol1/ch2/ch2_4/dao/UserDaoTest.java
+++ src/main/java/com/hideonbush/vol1/ch2/ch2_4/dao/UserDaoTest.java
@@ -7,13 +7,27 @@ import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.hideonbush.vol1.ch2.ch2_4.domain.User;
 
+// 스프링 테스트 컨텍스트 프레임워크
+// @RunWith - JUnit 프레임워크의 테스트 실행 방법을 확잘할때 사용
+// SpringJUnit4ClassRunner이라는 JUnit용 테스트 컨텍스트 프레임워크 확장 클래스
+// 이로 인해 JUnit이 테스트를 진행하는 중에 사용할 애플리케이션 컨텍스트를 생성, 관리하는 작업을 진행해준다
+
+// @ContextConfiguration - 만들 애플리케이션 컨텍스트의 설정파일 경로
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/com/hideonbush/vol1/ch2/ch2_4/applicationContext.xml")
 public class UserDaoTest {
+    @Autowired
+    private ApplicationContext context;
+
     private UserDao userDao;
     private User user1;
     private User user2;
@@ -21,8 +35,20 @@ public class UserDaoTest {
 
     @Before
     public void setup() {
-        ApplicationContext context = new GenericXmlApplicationContext(
-                "com/hideonbush/vol1/ch2/ch2_4/applicationContext.xml");
+        // 스프링의 JUnit 확장기능은
+        // 애플리케이션 컨텍스트를 테스트 실행전에 미리 만들어두고
+        // 테스트 객체가 생성될때마다 context 필드에 주입해준다
+        // 출력해보면 테스트 객체는 매 생성마다 주소값이 다른반면
+        // 컨텍스트는 한번 만들어진걸 계속 주입받아서 사용한다
+
+        // 스프링 테스트 컨텍스트 프레임워크 덕분에
+        // 한번 생성해둔 애플리케이션 컨텍스트를
+        // 매 테스트마다 재사용할 수 있게돼서 테스트 속도가 빨라짐
+        System.out.println();
+        System.out.println("애플리케이션 컨텍스트 -> " + this.context);
+        System.out.println();
+        System.out.println("UserDaoTest 객체 -> " + this);
+        System.out.println();
         this.userDao = context.getBean("userDao", UserDao.class);
 
         user1 = new User("jisung", "박지성", "jisungman");
