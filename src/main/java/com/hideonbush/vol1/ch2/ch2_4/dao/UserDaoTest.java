885424b414ff70afc208e6bad635b1d1805384fa
hideonbush3 <ppoii0961@naver.com>
2024-07-26 14:59:58 +0900

    2-4 @Autowired @Autowired 어노테이션을 걸어둔 인스턴스 변수가 있다면 애플리케이션 컨텍스트는 컨텍스트 내에서 해당 변수 타입과 일치하는 빈 객체를 찾아서 주입해준다

--- src/main/java/com/hideonbush/vol1/ch2/ch2_4/dao/UserDaoTest.java
+++ src/main/java/com/hideonbush/vol1/ch2/ch2_4/dao/UserDaoTest.java
@@ -9,7 +9,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -25,10 +24,20 @@ import com.hideonbush.vol1.ch2.ch2_4.domain.User;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/com/hideonbush/vol1/ch2/ch2_4/applicationContext.xml")
 public class UserDaoTest {
-    @Autowired
-    private ApplicationContext context;
+    // @Autowired
+    // 클래스를 인스턴스 변수로 두고 Autowired 어노테이션을 설정하면
+    // 애플리케이션 컨텍스트는 컨텍스트 내에서 타입과 일치하는 빈 객체를 주입해준다
 
+    // 만약,
+    // 타입과 일치하는 빈이 여러개일 경우
+    // 변수명과 빈 이름(id)이 같은 빈 객체를 주입해준다
+    // 변수명으로도 빈을 찾을 수 없는 경우 예외가 발생함
+
+    // UserDao도 빈으로 등록해뒀기 때문에 DL(getBean() 메서드 호출 방식)
+    // 방식으로 하지않고 바로 주입받을 수 있음
+    @Autowired
     private UserDao userDao;
+
     private User user1;
     private User user2;
     private User user3;
@@ -38,19 +47,10 @@ public class UserDaoTest {
         // 스프링의 JUnit 확장기능은
         // 애플리케이션 컨텍스트를 테스트 실행전에 미리 만들어두고
         // 테스트 객체가 생성될때마다 context 필드에 주입해준다
-        // 출력해보면 테스트 객체는 매 생성마다 주소값이 다른반면
-        // 컨텍스트는 한번 만들어진걸 계속 주입받아서 사용한다
 
         // 스프링 테스트 컨텍스트 프레임워크 덕분에
         // 한번 생성해둔 애플리케이션 컨텍스트를
         // 매 테스트마다 재사용할 수 있게돼서 테스트 속도가 빨라짐
-        System.out.println();
-        System.out.println("애플리케이션 컨텍스트 -> " + this.context);
-        System.out.println();
-        System.out.println("UserDaoTest 객체 -> " + this);
-        System.out.println();
-        this.userDao = context.getBean("userDao", UserDao.class);
-
         user1 = new User("jisung", "박지성", "jisungman");
         user2 = new User("ddangchil", "김땡칠", "ddangchilman");
         user3 = new User("victor", "빅터 차", "victorman");
