ad5c49666a55f95cbc6b4e888e7579b548e49c16
hideonbush3 <ppoii0961@naver.com>
2024-08-01 16:28:48 +0900

    2-5 학습테스트 예제 1 개선된 JUnit 테스트 객체 테스트 직전 테스트객체와 현재 테스트객체만 비교하던 허점을 갖고있던 테스트를 개선

--- src/main/java/com/hideonbush/vol1/learningtest/junit/JUnitTest.java
+++ src/main/java/com/hideonbush/vol1/learningtest/junit/JUnitTest.java
@@ -1,42 +1,34 @@
 package com.hideonbush.vol1.learningtest.junit;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
+// JUnit은 기본적으로 매 테스트 메서드마다 새로운 테스트 객체를 만들어서 테스트를 한다
+// 매 테스트마다 새로운 객체들이 생성되는것이 맞는지 확인해보기위한 학습테스트
 public class JUnitTest {
-    static JUnitTest testObject;
+    static Set<JUnitTest> testObjects = new HashSet<>();
 
-    // is()는 equals() 비교를 해서 같으면 성공, 다르면 실패
-    // not()은 결과를 부정하는 매처
-    // 즉, is(not())은 같으면 실패, 다르면 성공
-
-    // JUnitTest는 equals()를 오버라이드 하지 않았기 때문에
-    // 기본적으로 참조주소를 비교
-
-    // sameInstance()는 실제로 같은 객체인지 비교
-    // 객체참조를 비교하는 테스트의 의도를 명확히 드러내기위해
-    // sameInstance()라는 동일성 비교 매처를 명시적으로 사용
     @Test
     public void test1() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        // assertThat(this, not(is(testObject)));
-        // assertThat(this, not(sameInstance(testObject)));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test2() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test3() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 }
