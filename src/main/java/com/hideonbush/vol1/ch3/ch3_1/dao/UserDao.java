2b57caa6dc1c4b0d97dfa6cdb1a8e61ff6fa20f8
hideonbush3 <ppoii0961@naver.com>
2024-08-03 00:39:28 +0900

    3-1 예외처리기능 제한적인 리소스를 공유해 사용하는 JDBC 코드에는 예외 발생시에도 리소스를 반환하기 위해 예외처리 기능이 필수적이다 try/catch/finally

---
+++ src/main/java/com/hideonbush/vol1/ch3/ch3_1/dao/UserDao.java
@@ -0,0 +1,158 @@
+package com.hideonbush.vol1.ch3.ch3_1.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.hideonbush.vol1.ch3.ch3_1.domain.User;
+
+public class UserDao {
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource d) {
+        this.dataSource = d;
+    }
+
+    public void add(User user) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement(
+                    "insert into users(id, name, password) values(?,?,?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+    }
+
+    public User get(String id) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement(
+                    "select * from users where id = (?)");
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+
+            User user = null;
+
+            if (rs.next()) {
+                user = new User(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getString("password"));
+            }
+            if (user == null) {
+                throw new EmptyResultDataAccessException(1);
+            }
+            return user;
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.clearParameters();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+
+    }
+
+    public void deleteAll() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+
+    }
+
+    public int getCount() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("select count(*) from users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.clearParameters();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+    }
+
+}
