980fd25c1c5dac1cb2836b3edf4fcb5684eca063
hideonbush3 <ppoii0961@naver.com>
2024-08-03 00:36:02 +0900

    ch3 디렉토리 초기화

--- src/main/java/com/hideonbush/App.java
+++ src/main/java/com/hideonbush/App.java
@@ -4,6 +4,6 @@ import org.junit.runner.JUnitCore;
 
 public class App {
     public static void main(String[] args) {
-        JUnitCore.main("com.hideonbush.vol1.learningtest.junit.JUnitTest");
+        JUnitCore.main("com.hideonbush.vol1.ch3.ch3_1.dao.UserDaoTest");
     }
 }
