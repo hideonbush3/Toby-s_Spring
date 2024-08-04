5dded167149c60c0951de454bd7349e89d4720f1
hideonbush3 <ppoii0961@naver.com>
2024-08-04 18:10:24 +0900

    3.2.2 분리와 재사용을 위한 디자인 패턴 적용 DI 적용을 위한 클라이언트 / 컨텍스트 분리
    전략패턴에서 전략 오브젝트를 생성하는것은 일반적으로 클라이언트의 역할이다
    
    클라이언트?
    전략 오브젝트를 생성하고 컨텍스트를 호출하는 책임을 갖는다
    
    deleteAll()가 DeleteAllStatement 이라는 전략 클래스의 오브젝트를 생성하고 그것을 jdbcWithStatementStrategy() 라는 컨텍스트의 인자로 전달한다

--- src/main/java/com/hideonbush/vol1/ch3/ch3_2/dao/UserDao.java
+++ src/main/java/com/hideonbush/vol1/ch3/ch3_2/dao/UserDao.java
@@ -107,32 +107,16 @@ public abstract class UserDao {
     // 특정 확장기능(변하는 부분) -> PreparedStatement를 만드는 부분
     // Strategy에 해당하는것 -> StatementStrategy 인터페이스
     // 외부의 독립된 클래스에 해당하는것 -> DeleteAllStatement
+
+    // 전략패턴에서 전략 오브젝트 선정하고 생성해서 컨텍스트로 전달하는것은
+    // 일반적으로 클라이언트의 역할
+    // deleteAll()을 클라이언트로 만들었다
+    // DeleteAllStatement 이라는 전략 오브젝트를 생성해서
+    // jdbcWithStatementStrategy라는 컨텍스트를 호출한다
+    // 즉, 클라이언트는 전략 오브젝트를 생성하고 컨텍스트를 호출하는 책임을 갖는다
     public void deleteAll() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        try {
-            c = dataSource.getConnection();
-
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcWithStatementStrategy(st);
     }
 
     abstract protected PreparedStatement makeStatement(Connection c) throws SQLException;
@@ -159,6 +143,35 @@ public abstract class UserDao {
             if (ps != null) {
                 try {
                     ps.clearParameters();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+    }
+
+    // 컨텍스트
+    public void jdbcWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = dataSource.getConnection();
+
+            // 전략 오브젝트의 전략 호출(PreparedStatement를 만드는 외부 기능)
+            ps = stmt.makePreparedStatement(c);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
                 } catch (SQLException e) {
                 }
             }
