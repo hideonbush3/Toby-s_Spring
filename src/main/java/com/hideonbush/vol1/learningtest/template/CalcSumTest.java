70fa10075091b93076a1e3de8a9011edc346d41f
hideonbush3 <ppoii0961@naver.com>
2024-08-07 22:21:23 +0900

    3.5.3 템플릿/콜백의 응용 제네릭스를 이용한 콜백 인터페이스 기존엔 파일의 한 라인씩을 읽어와서 로직을 수행하고 Integer 타입으로 반환했었다
    만약, 파일의 한 라인씩 읽어온 값들을 하나의 문자열로 이어붙이려면?
    
    템플릿과 콜백의 반환타입이 String이여야 한다
    
    이런경우, 자바 5부터 지원된 제네릭스를 이용하면 된다
    
    파일의 라인을 읽어와서 처리를하고 T 타입의 결과를 만들어내는 범용적인 템플릿/콜백 패턴이 됐다
    
    처음엔 Integer 타입만 처리하는 템플릿/콜백 이였지만 여러 타입들을 다룰 수 있게돼서 더욱 확장에 유리한 구조가 됐다

--- src/main/java/com/hideonbush/vol1/learningtest/template/CalcSumTest.java
+++ src/main/java/com/hideonbush/vol1/learningtest/template/CalcSumTest.java
@@ -30,4 +30,9 @@ public class CalcSumTest {
     public void multiplyOfNumbers() throws IOException {
         assertThat(calculator.calcMultiply(numFilepath), is(24));
     }
+
+    @Test
+    public void concatenateStrings() throws IOException {
+        assertThat(calculator.concatenate(numFilepath), is("1234"));
+    }
 }
--- src/main/java/com/hideonbush/vol1/learningtest/template/Calculator.java
+++ src/main/java/com/hideonbush/vol1/learningtest/template/Calculator.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
         // 순수하게 계산에만 관심을 갖는 향상된 콜백
-        LineCallback sumCallback = new LineCallback() {
+        LineCallback<Integer> sumCallback = new LineCallback<Integer>() {
             public Integer doSomethingWithLine(String line, Integer value) {
                 return value + Integer.valueOf(line);
             }
@@ -16,12 +16,21 @@ public class Calculator {
     }
 
     public Integer calcMultiply(String filepath) throws IOException {
-        LineCallback multiplyCallback = new LineCallback() {
+        LineCallback<Integer> multiplyCallback = new LineCallback<Integer>() {
             public Integer doSomethingWithLine(String line, Integer value) {
                 return value * Integer.valueOf(line);
             }
         };
         return lineReadTemplate(filepath, multiplyCallback, 1);
+    }
+
+    public String concatenate(String filepath) throws IOException {
+        LineCallback<String> concateCallback = new LineCallback<String>() {
+            public String doSomethingWithLine(String line, String value) {
+                return value + line;
+            }
+        };
+        return lineReadTemplate(filepath, concateCallback, "");
     }
 
     // 템플릿
@@ -48,11 +57,13 @@ public class Calculator {
     // }
 
     // 콜백마다 중복되는 파일처리 코드까지 포함된 향상된 템플릿
-    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+    // 파일의 라인을 읽고 처리해서 T 타입의 결과를 만드는 범용적인 템플릿
+    // 여러 타입이 가능하기 때문에 다양하게 확장 가능해졌다
+    public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(filepath));
-            Integer res = initVal;
+            T res = initVal;
             String line = null;
             while ((line = br.readLine()) != null) {
                 res = callback.doSomethingWithLine(line, res);
