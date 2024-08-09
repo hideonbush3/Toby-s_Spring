b84f03b05e8be240595f5ee885a39f054a992220
hideonbush3 <ppoii0961@naver.com>
2024-08-09 23:11:44 +0900

    3.6.4 query() query()를 이용한 getAll() 구현 테스트 코드를 먼저 작성한다

--- src/main/java/com/hideonbush/vol1/ch3/ch3_6/dao/UserDaoTest.java
+++ src/main/java/com/hideonbush/vol1/ch3/ch3_6/dao/UserDaoTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -69,5 +70,34 @@ public class UserDaoTest {
         assertThat(userDao.getCount(), is(0));
 
         userDao.get("abc");
+    }
+
+    @Test
+    public void getAll() throws SQLException {
+        userDao.deleteAll();
+
+        userDao.add(user1);
+        List<User> users1 = userDao.getAll();
+        assertThat(users1.size(), is(1));
+        checkSameUser(user1, users1.get(0));
+
+        userDao.add(user2);
+        List<User> users2 = userDao.getAll();
+        assertThat(users2.size(), is(2));
+        checkSameUser(user2, users2.get(0));
+        checkSameUser(user1, users2.get(1));
+
+        userDao.add(user3);
+        List<User> users3 = userDao.getAll();
+        assertThat(users3.size(), is(3));
+        checkSameUser(user2, users3.get(0));
+        checkSameUser(user1, users3.get(1));
+        checkSameUser(user3, users3.get(2));
+    }
+
+    private void checkSameUser(User user1, User user2) {
+        assertThat(user1.getId(), is(user2.getId()));
+        assertThat(user1.getName(), is(user2.getName()));
+        assertThat(user1.getPassword(), is(user2.getPassword()));
     }
 }
