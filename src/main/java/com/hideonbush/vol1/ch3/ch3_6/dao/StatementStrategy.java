10c0fddfac842261266ea0bebe06920b4a76a9b6
hideonbush3 <ppoii0961@naver.com>
2024-08-12 21:40:22 +0900

    3장 JdbcTemplate를 사용하면서 필요없게 됨

--- src/main/java/com/hideonbush/vol1/ch3/ch3_6/dao/StatementStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.hideonbush.vol1.ch3.ch3_6.dao;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface StatementStrategy {
-    public PreparedStatement makePreparedStatement(Connection c) throws SQLException;
-}
