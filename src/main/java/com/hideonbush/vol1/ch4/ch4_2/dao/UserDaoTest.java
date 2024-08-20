b8e559a04d1ed01fc5407d05829a7c85dfe2b99b
hideonbush3 <ppoii0961@naver.com>
2024-08-20 19:52:10 +0900

    4.2.4 기술에 독립적인 UserDao 만들기 테스트 보완

--- src/main/java/com/hideonbush/vol1/ch4/ch4_2/dao/UserDaoTest.java
+++ src/main/java/com/hideonbush/vol1/ch4/ch4_2/dao/UserDaoTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,7 +20,7 @@ import com.hideonbush.vol1.ch4.ch4_2.domain.User;
 @ContextConfiguration(locations = "/com/hideonbush/vol1/ch4/test-applicationContext.xml")
 public class UserDaoTest {
     @Autowired
-    private UserDao userDao;
+    private UserDao dao;
 
     private User user1;
     private User user2;
@@ -34,67 +35,63 @@ public class UserDaoTest {
 
     @Test
     public void addAndGet() {
-        userDao.deleteAll();
-        assertThat(userDao.getCount(), is(0));
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
-        userDao.add(user1);
-        userDao.add(user2);
-        assertThat(userDao.getCount(), is(2));
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        User savedUser1 = userDao.get(user1.getId());
+        User savedUser1 = dao.get(user1.getId());
         assertThat(savedUser1.getName(), is(user1.getName()));
         assertThat(savedUser1.getPassword(), is(user1.getPassword()));
 
-        User savedUser2 = userDao.get(user2.getId());
+        User savedUser2 = dao.get(user2.getId());
         assertThat(savedUser2.getName(), is(user2.getName()));
         assertThat(savedUser2.getPassword(), is(user2.getPassword()));
     }
 
     @Test
     public void count() {
-        userDao.deleteAll();
-        assertThat(userDao.getCount(), is(0));
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
-        userDao.add(user1);
-        assertThat(userDao.getCount(), is(1));
-        userDao.add(user2);
-        assertThat(userDao.getCount(), is(2));
-        userDao.add(user3);
-        assertThat(userDao.getCount(), is(3));
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
     }
 
     @Test(expected = EmptyResultDataAccessException.class)
     public void getUserFailure() {
-        userDao.deleteAll();
-        assertThat(userDao.getCount(), is(0));
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
-        userDao.get("abc");
+        dao.get("abc");
     }
 
     @Test
     public void getAll() {
-        userDao.deleteAll();
+        dao.deleteAll();
 
-        // userDao.getAll()에 대한 테스트 코드인 동시에
-        // 기능을 설명해주는 코드
-        // 여기서 기능이란, 아무런 데이터가 없을때 userDao.getAll()를
-        // 호출하면 크기가 0인 List<T>를 반환한다는 것을 뜻한다
-        List<User> users0 = userDao.getAll();
+        List<User> users0 = dao.getAll();
         assertThat(users0.size(), is(0));
 
-        userDao.add(user1);
-        List<User> users1 = userDao.getAll();
+        dao.add(user1);
+        List<User> users1 = dao.getAll();
         assertThat(users1.size(), is(1));
         checkSameUser(user1, users1.get(0));
 
-        userDao.add(user2);
-        List<User> users2 = userDao.getAll();
+        dao.add(user2);
+        List<User> users2 = dao.getAll();
         assertThat(users2.size(), is(2));
         checkSameUser(user2, users2.get(0));
         checkSameUser(user1, users2.get(1));
 
-        userDao.add(user3);
-        List<User> users3 = userDao.getAll();
+        dao.add(user3);
+        List<User> users3 = dao.getAll();
         assertThat(users3.size(), is(3));
         checkSameUser(user2, users3.get(0));
         checkSameUser(user1, users3.get(1));
@@ -105,5 +102,19 @@ public class UserDaoTest {
         assertThat(user1.getId(), is(user2.getId()));
         assertThat(user1.getName(), is(user2.getName()));
         assertThat(user1.getPassword(), is(user2.getPassword()));
+    }
+
+    // 중복된 키를 가진 정보를 추가했을때 어떤 예외가 발생하는지 확인하기 위한 테스트
+    // 스프링이 데이터 액세스 예외를 다루는 기능을 확인하기 위한 것이니 일종의 학습테스트
+    // 스프링의 DataAccessException 예외 중 하나가 던져져야 한다
+    // DataAccessException의 서브 클래스인
+    // DataIntegrityViolationException의 서브 클래스인
+    // DuplicateKeyException가 발생한다
+    @Test(expected = DuplicateKeyException.class)
+    public void duplicateKey() {
+        dao.deleteAll();
+
+        dao.add(user1);
+        dao.add(user1);
     }
 }
