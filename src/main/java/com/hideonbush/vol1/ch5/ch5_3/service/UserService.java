8d55bd29ed4cff6dd28b051146e7eb5f8c4ee381
hideonbush3 <ppoii0961@naver.com>
2024-08-26 17:12:09 +0900

    5.1.3 UserService.upgradeLevels() UserService 클래스와 빈 등록
    유저 정보를 변경하는 update() 메서드까지 테스트를 마쳤고, 이젠
    사용자 관리 비지니스 로직을 추가할 차례이다
    
    DAO는 데이터를 어떻게 가져오고 조작하는지를 담당하는 곳이다
    
    비지니스 로직을 담기에는 어울리지 않는다
    
    비지니스 로직 서비스를 제공한다는 의미에서 UserService 라는 클래스에 사용자 관리 로직을 담을 것이다
    
    데이터 액세스 방식이 달라진다고 비지니스 로직이 달라지면 안된다
    
    그렇기 때문에 UserService는 UserDao를 DI 받도록 만든다
    
    UserService가 DI를 받으려면 UserService도 당연히 스프링 빈으로 등록돼야 한다

---
+++ src/main/java/com/hideonbush/vol1/ch5/ch5_3/service/UserService.java
@@ -0,0 +1,11 @@
+package com.hideonbush.vol1.ch5.ch5_3.service;
+
+import com.hideonbush.vol1.ch5.ch5_3.dao.UserDao;
+
+public class UserService {
+    UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+}
