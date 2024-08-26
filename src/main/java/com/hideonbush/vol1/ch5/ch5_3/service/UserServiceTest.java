43a76161a9b0a8a9f1145dcf13920aaf27d6059e
hideonbush3 <ppoii0961@naver.com>
2024-08-26 20:38:25 +0900

    5.1.3 UserService.upgradeLevels() upgradeLevels() 테스트

--- src/main/java/com/hideonbush/vol1/ch5/ch5_3/service/UserServiceTest.java
+++ src/main/java/com/hideonbush/vol1/ch5/ch5_3/service/UserServiceTest.java
@@ -1,14 +1,21 @@
 package com.hideonbush.vol1.ch5.ch5_3.service;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.hideonbush.vol1.ch5.ch5_3.dao.UserDao;
+import com.hideonbush.vol1.ch5.ch5_3.domain.Level;
+import com.hideonbush.vol1.ch5.ch5_3.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/com/hideonbush/vol1/ch5/test-applicationContext.xml")
@@ -16,8 +23,40 @@ public class UserServiceTest {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserDao userDao;
+
+    List<User> users;
+
+    @Before
+    public void setUp() {
+        users = Arrays.asList(
+                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
+                new User("erwins", "강명성", "p2", Level.BASIC, 50, 0),
+                new User("joytouch", "신승한", "p3", Level.SILVER, 60, 29),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
+                new User("green", "오민규", "p5", Level.GOLD, 100, 100));
+    }
+
     @Test
-    public void bean() {
-        assertThat(userService, is(notNullValue()));
+    public void upgradeLevels() {
+        userDao.deleteAll();
+
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        assertThat(userUpdate.getLevel(), is(expectedLevel));
     }
 }
