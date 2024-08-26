f0fcf82d82a5e89b6f0092169840bed0d33e8797
hideonbush3 <ppoii0961@naver.com>
2024-08-26 16:54:48 +0900

    5.1.3 UserService.upgradeLevels() 디렉토리 초기화

--- src/main/java/com/hideonbush/App.java
+++ src/main/java/com/hideonbush/App.java
@@ -4,6 +4,6 @@ import org.junit.runner.JUnitCore;
 
 public class App {
     public static void main(String[] args) {
-        JUnitCore.main("com.hideonbush.vol1.ch5.ch5_2.dao.UserDaoTest");
+        JUnitCore.main("com.hideonbush.vol1.ch5.ch5_3.dao.UserDaoTest");
     }
 }
