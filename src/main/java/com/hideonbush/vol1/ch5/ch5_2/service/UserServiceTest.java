750bf0f00cb567dda6328b6079177a770c414f22
hideonbush3 <ppoii0961@naver.com>
2024-08-27 19:39:55 +0900

    5.2 트랜잭션 서비스 추상화 디렉토리 초기화

---
+++ src/main/java/com/hideonbush/vol1/ch5/ch5_2/service/UserServiceTest.java
@@ -0,0 +1,86 @@
+package com.hideonbush.vol1.ch5.ch5_2.service;
+
+import static com.hideonbush.vol1.ch5.ch5_2.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
+import static com.hideonbush.vol1.ch5.ch5_2.service.UserService.MIN_RECOMMEND_FOR_GOLD;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.hideonbush.vol1.ch5.ch5_2.dao.UserDao;
+import com.hideonbush.vol1.ch5.ch5_2.domain.Level;
+import com.hideonbush.vol1.ch5.ch5_2.domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/com/hideonbush/vol1/ch5/test-applicationContext.xml")
+public class UserServiceTest {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserDao userDao;
+
+    List<User> users;
+
+    @Before
+    public void setUp() {
+        users = Arrays.asList(
+                new User("bumjin", "박범진", "p1", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER - 1, 0),
+                new User("erwins", "강명성", "p2", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER, 0),
+                new User("joytouch", "신승한", "p3", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD - 1),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD),
+                new User("green", "오민규", "p5", Level.GOLD, 100, Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void upgradeLevels() {
+        userDao.deleteAll();
+
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevelUpgraded(users.get(0), false);
+        checkLevelUpgraded(users.get(1), true);
+        checkLevelUpgraded(users.get(2), false);
+        checkLevelUpgraded(users.get(3), true);
+        checkLevelUpgraded(users.get(4), false);
+    }
+
+    private void checkLevelUpgraded(User user, boolean upgraded) {
+        User userUpdate = userDao.get(user.getId());
+        if (upgraded) {
+            assertThat(userUpdate.getLevel(), is(user.getLevel().nextLevel()));
+        } else {
+            assertThat(userUpdate.getLevel(), is(user.getLevel()));
+        }
+    }
+
+    public void add() {
+        userDao.deleteAll();
+
+        User userWithLevel = users.get(4);
+        User userWithoutLevel = users.get(0);
+        userWithoutLevel.setLevel(null);
+
+        userService.add(userWithLevel);
+        userService.add(userWithoutLevel);
+
+        User userWithLevelRead = userDao.get(userWithLevel.getId());
+        User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+
+        assertThat(userWithLevel.getLevel(), is(userWithLevelRead.getLevel()));
+        assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
+    }
+}
