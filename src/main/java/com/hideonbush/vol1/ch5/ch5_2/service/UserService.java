88b4ba0c36182da74e77a9621a4ab174839597d9
hideonbush3 <ppoii0961@naver.com>
2024-08-27 22:44:09 +0900

    5.2.2 트랜잭션 경계설정 트랜잭션 동기화 적용
    트랜잭션을 적용하는 방법이 있다
    
    커넥션의 오토커밋 기능을 끄면 트랜잭션이 시작되고 커넥션의 롤백이나 커밋 메서드를 호출하면 트랜잭션이 종료된다
    
    이렇게 트랜잭션의 시작되는 부분과 끝나는 부분을 트랜잭션 경계라고한다
    
    JDBC의 트랜잭션 경계 설정 메서드는 코두 Connection 오브젝트에 포함돼있는데 JdbcTemplate를 사용한 이후부터 이 Connection 객체는 구경도 못했다
    
    JdbcTemplate의 작업흐름은 직접 만들었던 JdbcContext와 거의 동일하다
    
    Connection 객체를 가져와 작업하고 작업이 끝나면 Connection을 닫아주고 템플릿을 빠져나온다
    
    일반적으로 트랜잭션은 커넥션보다 존재 범위가 짧다
    
    JdbcTemplate를 사용하는 UserDao는 각 메서드마다 독립적인 트랜잭션으로 실행될 수밖에 없다
    
    UserService의 upgradeLevels() 같이 DB에 여러번 업데이트를 하는 작업을 하나의 트랜잭션으로 묶으려면 사용되는 커넥션 객체도 하나여야 할 것이다
    
    이를 위해 스프링에서 제안하는 방식은 트랜잭션 동기화 방식이다
    
    트랜잭션 동기화란 트랜잭션을 시작하려는 곳에서 만든 Connection 객체를 특별한 장소에 저장해두고 이후에 호출되는 DAO가 사용하는 JdbcTemplate이 해당 Connection 객체를 사용하도록 하는 것이다
    
    스프링은 트랜잭션 동기화 기능을 지원하는 간단한 유틸리미 메서드를 제공한다

--- src/main/java/com/hideonbush/vol1/ch5/ch5_2/service/UserService.java
+++ src/main/java/com/hideonbush/vol1/ch5/ch5_2/service/UserService.java
@@ -1,6 +1,12 @@
 package com.hideonbush.vol1.ch5.ch5_2.service;
 
+import java.sql.Connection;
 import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import com.hideonbush.vol1.ch5.ch5_2.dao.UserDao;
 import com.hideonbush.vol1.ch5.ch5_2.domain.Level;
@@ -8,6 +14,7 @@ import com.hideonbush.vol1.ch5.ch5_2.domain.User;
 
 public class UserService {
     UserDao userDao;
+    private DataSource dataSource;
 
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
     public static final int MIN_RECOMMEND_FOR_GOLD = 30;
@@ -16,13 +23,47 @@ public class UserService {
         this.userDao = userDao;
     }
 
-    // 트랜잭션이 적용되지 않은 메서드
-    public void upgradeLevels() {
-        List<User> users = userDao.getAll();
-        for (User user : users) {
-            if (canUpgradeLevel(user)) {
-                upgradeLevel(user);
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    // 트랜잭션 동기화가 돼있는 상태에서 JdbcTemplate을 사용하면
+    // JdbcTemplate은 동기화 돼있는 DB 커넥션을 사용한다
+    // 결국 DAO를 통해 진행되는 모든 데이터 액세스 작업은
+    // upgradeLevels() 메서드에서 만든 똑같은 Connection을 사용한다
+    public void upgradeLevels() throws Exception {
+        // 스프링이 제공하는 동기화 관리 클래스
+        // 트랜잭션 동기화 작업 초기화 요청
+        TransactionSynchronizationManager.initSynchronization();
+
+        // 스프링이 제공하는 유틸 클래스 DataSourceUtils
+        // DataSource에서 커넥션을 가져오지 않고 DataSourceUtils에서 가져온다
+        // DataSourceUtils에서 커넥션을 가져오면 해당 커넥션을
+        // 트랜잭션 동기화에 사용하도록 저장소에 자동으로 바인딩 해준다
+        Connection c = DataSourceUtils.getConnection(dataSource);
+
+        // 트랜잭션 시작
+        c.setAutoCommit(false);
+        try {
+            List<User> users = userDao.getAll();
+            for (User user : users) {
+                if (canUpgradeLevel(user)) {
+                    upgradeLevel(user);
+                }
             }
+
+            // 작업 정상종료시 트랜잭션 커밋
+            c.commit();
+        } catch (Exception e) {
+            // 이상 발생시 원상복구
+            c.rollback();
+            throw e;
+        } finally {
+            DataSourceUtils.releaseConnection(c, dataSource); // DB 커넥션 닫기
+
+            // 동기화 작업 종료 및 정리
+            TransactionSynchronizationManager.unbindResource(dataSource);
+            TransactionSynchronizationManager.clearSynchronization();
         }
     }
 
