57951ea02b0055191289b7993582994bd759c303
hideonbush3 <ppoii0961@naver.com>
2024-09-04 18:07:44 +0900

    5.4.1 JavaMail을 이용한 메일 발송 기능 JavaMail 메일 발송

--- src/main/java/com/hideonbush/vol1/ch5/ch5_4/service/UserService.java
+++ src/main/java/com/hideonbush/vol1/ch5/ch5_4/service/UserService.java
@@ -1,6 +1,17 @@
 package com.hideonbush.vol1.ch5.ch5_4.service;
 
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
@@ -76,5 +87,43 @@ public class UserService {
     protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEmail(user);
+    }
+
+    private void sendUpgradeEmail(User user) {
+        String adminEmailAddress = "이메일";
+        String adminEmailPassword = "비밀번호";
+        String smtpHost = "smtp.gmail.com";
+        int smtpPort = 465;
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", smtpPort);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.ssl.trust", smtpHost);
+
+        Session s = Session.getInstance(props,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(adminEmailAddress, adminEmailPassword);
+                    }
+                });
+        MimeMessage message = new MimeMessage(s);
+        try {
+            message.setFrom(new InternetAddress(adminEmailAddress));
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이 " + user.getLevel().name() +
+                    "로 업그레이드 됐습니다");
+
+            Transport.send(message);
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
