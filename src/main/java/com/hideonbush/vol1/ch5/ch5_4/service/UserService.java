eabc219eeba068e760c2a40686eb5c34028ba4c1
hideonbush3 <ppoii0961@naver.com>
2024-09-05 19:52:59 +0900

    5.4.3 테스트를 위한 서비스 추상화 메일 발송 기능 추상화
    JavaMail은 자바의 표준 기술이고 이미 수많은 시스템에서 검증이 된 안정적인 모듈이다
    
    그렇기때문에 메일 발송 테스트를 실행했을 JavaMail API를 통해 요청이 들어가는 것 까지만 확인하면 되지 JavaMail을 직접 구동시킬 필요는 없다
    
    그래서 테스트할땐 JavaMail과 같은 인터페이스를 갖는 오브젝트를 사용하면 될것이다
    
    하지만 JavaMail은 확장이나 지원이 불가능하도록 만들어진 악명높은 표준 API중 하나이다
    
    스프링은 JavaMail을 사용해 만든 코드는 쉽게 테스트하기 힘들다는 문제를 해결하기 위해 JavaMail에 대한 추상화 기능을 제공하고 있다
    
    스프링은 메일 서비스 추상화의 핵심 인터페이스인 MailSender를 제공한다

--- src/main/java/com/hideonbush/vol1/ch5/ch5_4/service/UserService.java
+++ src/main/java/com/hideonbush/vol1/ch5/ch5_4/service/UserService.java
@@ -3,16 +3,8 @@ package com.hideonbush.vol1.ch5.ch5_4.service;
 import java.util.List;
 import java.util.Properties;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -91,39 +83,33 @@ public class UserService {
     }
 
     private void sendUpgradeEmail(User user) {
+        String smtpHost = "smtp.gmail.com";
+
+        Properties javaMailProperties = new Properties();
+        javaMailProperties.put("mail.smtp.auth", true);
+        javaMailProperties.put("mail.smtp.starttls.enable", true);
+        javaMailProperties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
         String adminEmailAddress = "이메일";
         String adminEmailPassword = "비밀번호";
-        String smtpHost = "smtp.gmail.com";
-        int smtpPort = 465;
 
-        Properties props = new Properties();
-        props.put("mail.smtp.host", smtpHost);
-        props.put("mail.smtp.port", smtpPort);
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
-        props.put("mail.smtp.ssl.enable", "true");
-        props.put("mail.smtp.ssl.trust", smtpHost);
+        // MailSender의 구현 클래스인 JavaMailSenderImpl
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(smtpHost);
+        mailSender.setJavaMailProperties(javaMailProperties);
+        mailSender.setProtocol("smtp");
+        mailSender.setPort(587);
+        mailSender.setUsername(adminEmailAddress);
+        mailSender.setPassword(adminEmailPassword);
 
-        Session s = Session.getInstance(props,
-                new Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(adminEmailAddress, adminEmailPassword);
-                    }
-                });
-        MimeMessage message = new MimeMessage(s);
-        try {
-            message.setFrom(new InternetAddress(adminEmailAddress));
-            message.addRecipient(Message.RecipientType.TO,
-                    new InternetAddress(user.getEmail()));
-            message.setSubject("Upgrade 안내");
-            message.setText("사용자님의 등급이 " + user.getLevel().name() +
-                    "로 업그레이드 됐습니다");
+        // 간단한 텍스트가 전부이므로 MailMessage를 구현한
+        // SimpleMailMessage를 사용해 메시지 생성
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setFrom(adminEmailAddress);
+        mailMessage.setSubject("Upgrade 안내");
+        mailMessage.setText("사용자님의 등급이 " + user.getLevel().name());
 
-            Transport.send(message);
-        } catch (AddressException e) {
-            throw new RuntimeException(e);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+        mailSender.send(mailMessage);
     }
 }
