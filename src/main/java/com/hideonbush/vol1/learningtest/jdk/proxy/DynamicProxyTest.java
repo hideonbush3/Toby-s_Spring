1f696408b41706aa4a7c12d15b661891526d6482
hideonbush3 <ppoii0961@naver.com>
2024-09-16 21:06:40 +0900

    6.3.2 다이내믹 프록시 다이내믹 프록시 적용
    다이내믹 프록시는 프록시 팩토리에 의해 런타임 시 동적으로 생성되는 오브젝트다
    
    프록시 팩토리에게 인터페이스에 대한 정보를 제공하면 해당 인터페이스를 구현한 오브젝트를 자동으로 만들더준다
    
    그렇기 때문에 프록시를 만들때 인터페이스를 모두 구현해야했던 수고를 덜 수 있다
    
    다이내믹 프록시가 인터페이스 구현 클래스의 객체는 만들어주지만 프록시로써 필요한 부가기능 제공 코드는 직접 작성해야 한다
    
    이 부가기능은 invoke() 라는 단 하나의 메서드만 담고있는 InvocationHandler를 구현한 클래스에 담는다
    
    다이내믹 프록시 오브젝트는 클라이언트의 모든 요청을 리플렉션 정보로 변환해서 InvocationHandler 구현 객체의 invoke() 메서드로 넘긴다
    
    직접 정의해서 만든 프록시 클래스인 HelloUppercase는 Hello의 모든 메서드를 직접 구현해야했고 부가기능이 메서드마다 중복됐었는데 다이내믹 프록시를 사용하며 이런 문제점들이 번거로움이 해결됐다

--- src/main/java/com/hideonbush/vol1/learningtest/jdk/proxy/DynamicProxyTest.java
+++ src/main/java/com/hideonbush/vol1/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -3,6 +3,10 @@ package com.hideonbush.vol1.learningtest.jdk.proxy;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 import org.junit.Test;
 
 public class DynamicProxyTest {
@@ -14,14 +18,18 @@ public class DynamicProxyTest {
         assertThat(hello.sayHi("Minsu"), is("Hi Minsu"));
         assertThat(hello.sayThankYou("Minsu"), is("Thank You Minsu"));
 
-        // 프록시 테스트
-        Hello proxiedHello = new HelloUppercase(hello);
+        // 다이내믹 프록시 생성
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(), // 동적 프록시 생성시 필요한 클래스 정보를 로드할 클래스 로더
+                new Class[] { Hello.class }, // 동적 프록시가 구현해야할 인터페이스, 여러개가 될 수 있어서 배열
+                new UppercaseHandler(new HelloTarget())); // 프록시 객체가 타깃의 메서드를 호출할때 이를 가로챌 InvocationHandler
+        // Hello proxiedHello = new HelloUppercase(hello);
         assertThat(proxiedHello.sayHello("Minsu"), is("HELLO MINSU"));
         assertThat(proxiedHello.sayHi("Minsu"), is("HI MINSU"));
         assertThat(proxiedHello.sayThankYou("Minsu"), is("THANK YOU MINSU"));
     }
 
-    public class HelloUppercase implements Hello {
+    static class HelloUppercase implements Hello {
         Hello hello;
 
         public HelloUppercase(Hello hello) {
@@ -39,7 +47,21 @@ public class DynamicProxyTest {
         public String sayThankYou(String name) {
             return hello.sayThankYou(name).toUpperCase();
         }
+    }
 
+    // 다이내믹 프록시가 타깃의 메서드를 호출할때 이를 가로챌 InvocationHandler
+    // 타깃의 반환값을 대문자로 바꾸는 부가기능과 요청 위임 코드를 담고있다
+    static class UppercaseHandler implements InvocationHandler {
+        Hello target;
+
+        private UppercaseHandler(Hello target) {
+            this.target = target;
+        }
+
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            String ret = (String) method.invoke(target, args);
+            return ret.toUpperCase();
+        }
     }
 
     static interface Hello {
