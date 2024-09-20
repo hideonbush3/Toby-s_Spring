7369f7dd2774c6f3ab5ebc371af967c685a94e3e
hideonbush3 <ppoii0961@naver.com>
2024-09-20 16:36:54 +0900

    6.3.4 다이내믹 프록시를 위한 팩토리 빈 트랜잭션 프록시 팩토리 빈
    팩토리 빈은 다이내믹 프록시가 위임할 타깃 오브젝트의 참조를 프로퍼티를 통해 DI 받아야 한다
    
    다이내믹 프록시와 함께 생성할 InvocationHandler에게 타깃 오브젝트를 전달해줘야 하기 때문이다
    
    UserServiceTx는 이제 더이상 필요없다
    
    Class 타입은 value를 이용해 클래스 또는 인터페이스의 이름을 넣어주면 된다
    
    스프링은 수정자 메소드의 파라미터의 타입을 확인해서 프로퍼티의 타입이 Class인 경우는 value로 설정한 이름을 가진 Class 오브젝트로 자동 변환해준다

---
+++ src/main/java/com/hideonbush/vol1/ch6/ch6_3/service/TxProxyFactoryBean.java
@@ -0,0 +1,59 @@
+package com.hideonbush.vol1.ch6.ch6_3.service;
+
+import java.lang.reflect.Proxy;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+// UserService 외에도 트랜잭션 부가기능이 필요한 오브젝트를 위한
+// 프록시를 만들 때 얼마든지 재사용 가능하다 
+public class TxProxyFactoryBean implements FactoryBean<Object> {
+    // TransactionHandler를 생성할때 필요한 리스트
+    Object target;
+    PlatformTransactionManager transactionManager;
+    String pattern;
+
+    // 다이내믹 프록시를 생성할때 필요
+    Class<?> serviceInterface;
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public void setServiceInterface(Class<?> serviceInterface) {
+        this.serviceInterface = serviceInterface;
+    }
+
+    // DI 받은 정보를 이용해서 TransactionHandler를 사용하는
+    // 다이내믹 프록시를 생성한다
+    @Override
+    public Object getObject() throws Exception {
+        TransactionHandler txHandler = new TransactionHandler();
+        txHandler.setTarget(target);
+        txHandler.setTransactionManager(transactionManager);
+        txHandler.setPattern(pattern);
+        return Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[] { serviceInterface },
+                txHandler);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return this.serviceInterface;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false; // 싱글톤이 아니라는 뜻이 아니라 getObject()는 매번 다른 오브젝트를 리턴한다
+    }
+
+}
